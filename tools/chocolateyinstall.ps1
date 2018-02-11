81d119194176fbcbfee54fa17da62ab2a413d372
David Gardiner <david@gardiner.net.au>
2018-02-11 19:52:36 +1030

    Update for version 5.07.0070
    Fixes #1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'lenovo-thinkvantage-system-update'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.lenovo.com/pccbbs/thinkvantage_en/systemupdate5.07.0065.exe'
+$url        = 'https://download.lenovo.com/pccbbs/thinkvantage_en/systemupdate5.07.0070.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'Lenovo System Update'
-  checksum      = '6C5FAEC8020F9C40768FCF2AFC339C51F0C384710B748B9BA6A8C126FC895A06'
+  checksum      = '0add129e812bfd2779d5f1d86909b88fc949f12bb51c040fed4134655dac8045'
   checksumType  = 'sha256'
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
